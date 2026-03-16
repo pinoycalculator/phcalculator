fd3e44721c39305b864cf945ca2eef3571624a60
pinoycalculator <aiworksfedexbdgg@gmail.com>
2026-03-16 17:34:27 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ function toggleMenu(){
 
 let menu = document.getElementById("navMenu");
 
+if(menu){
 menu.classList.toggle("active");
+}
 
 }
+
+
+/* LOAD HEADER AND FOOTER */
+
+function loadComponent(id, file){
+
+fetch(file)
+.then(response => response.text())
+.then(data => {
+document.getElementById(id).innerHTML = data;
+});
+
+}
+
+loadComponent("header","components/header.html");
+loadComponent("footer","components/footer.html");
